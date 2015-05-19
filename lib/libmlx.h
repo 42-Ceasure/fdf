81df713652b1f6d766cd2f78e4e98ed2587c5923
42-Ceasure <m-one_laz@hotmail.fr>
2015-05-19 13:02:38 -0700

    Update libmlx.h

--- lib/libmlx.h
+++ lib/libmlx.h
@@ -13,12 +13,12 @@
 #ifndef LIBMLX_H
 # define LIBMLX_H
 
-# include <mlx.h>
 # include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include "minilibx_macos/mlx.h"
 # include "libft/libft.h"
 # define WIDTH 1920
 # define HEIGHT 1080
